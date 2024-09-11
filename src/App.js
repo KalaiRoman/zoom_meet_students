25d21c86db1479b39a60bf638403787216f68ddc
kalai <kalai@cdp360.com>
2024-09-11 16:52:36 +0530

    live url change in socket

--- src/App.js
+++ src/App.js
@@ -1,47 +1,38 @@
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
 import Routing from './routing/Routing';
 import { io } from 'socket.io-client';
 import { useDispatch } from 'react-redux';
 import { addSocket } from './redux/Reducers/socket_reducer';
 
-
-
 function App() {
-
-
-  const dispatch=useDispatch();
-
-  const socketConnection = io('https://flipkart-clone-2vnu.onrender.com',{
-    transports: ['websocket'], 
-    withCredentials: true,  
-  }); 
-
-
+  const dispatch = useDispatch();
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-   
+    const socketConnection = io('https://flipkart-clone-2vnu.onrender.com', {
+      transports: ['websocket'],
+      withCredentials: true,
+    });
 
     setSocket(socketConnection);
 
     socketConnection.on('connect', () => {
-      console.log('Connected with Socket ID:', socketConnection.id); 
+      console.log('Connected with Socket ID:', socketConnection.id);
     });
 
-   
     socketConnection.on('disconnect', () => {
       console.log('Socket disconnected');
     });
 
-    dispatch(addSocket(socketConnection))
+    dispatch(addSocket(socketConnection));
 
     return () => {
       socketConnection.disconnect();
       console.log('Socket disconnected on component unmount');
     };
+  }, [dispatch]); 
 
-  }, [socketConnection]); 
   return (
     <div className="App">
       <Routing />
